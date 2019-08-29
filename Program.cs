4c1553094b23151ed1cfbaa091119a55b6faa42a
Cristian Malaga <cmalaga@usf.edu>
2019-08-29 19:43:21 -0400

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+namespace HelloWorld
+{
+    class Hello
+    {
+        static void Main(string[] args)
+        {
+            //Declared a string variable for use later in the program
+            string myName = "Cristian Malaga";
+            //Displayed the output and concatenated the string output
+            Console.WriteLine("Hello World! My name is " + myName);
+        }
+    }
+}
